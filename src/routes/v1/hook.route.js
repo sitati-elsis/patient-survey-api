a215966fff549df7009ab4d6ee1f97fcdc2a0a43
Norbert Glen <norbertglen7@gmail.com>
2023-05-23 22:45:48 +0300

    mirth route

--- src/routes/v1/hook.route.js
+++ src/routes/v1/hook.route.js
@@ -6,7 +6,7 @@ router
   .route("/mirth")
   .post( async (req, res) => {
     console.log(req.body)
-    res.send({message: "success"});
+    res.send({message: req.body});
   });
 
 module.exports = router;
