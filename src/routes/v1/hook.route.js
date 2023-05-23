38abb619202dd8252326bb4584b1ae2f1eb9852b
Norbert Glen <norbertglen7@gmail.com>
2023-05-24 00:16:27 +0300

    mirth events basic auth setup

--- src/routes/v1/hook.route.js
+++ src/routes/v1/hook.route.js
@@ -5,6 +5,33 @@ const router = express.Router();
 
 router
   .route("/mirth")
-  .post(eventController.createEvent);
+  .post(function authentication(req, res, next) {
+    const authheader = req.headers.authorization;
+    console.log(req.headers);
+ 
+    if (!authheader) {
+        let err = new Error('You are not authenticated!');
+        res.setHeader('WWW-Authenticate', 'Basic');
+        err.status = 401;
+        return next(err)
+    }
+ 
+    const auth = new Buffer.from(authheader.split(' ')[1],
+        'base64').toString().split(':');
+    const user = auth[0];
+    const pass = auth[1];
+ 
+    if (user == process.env.MIRTH_USER && pass == process.env.MIRTH_API_KEY) {
+ 
+        // If Authorized user
+        next();
+    } else {
+        let err = new Error('You are not authenticated!');
+        res.setHeader('WWW-Authenticate', 'Basic');
+        err.status = 401;
+        return next(err);
+    }
+ 
+}, eventController.createEvent);
 
 module.exports = router;
