11db613ad8254f1c6301e0ffce783cc457a16150
Norbert Glen <norbert@jnngroup.com>
2023-05-25 13:57:16 +0000

    Merged in settings-page-integration (pull request #8)
    Settings page integration
    
    * draft settings api
    
    * merge master
    
    * updating organization
    
    * organization operation schedule and amenitites
    
    * merge master

---
+++ src/config/features.js
@@ -0,0 +1,11 @@
+const amenities = [
+  'Valet parking',
+  'Drive-through Covid Testing',
+  'Parking Available',
+  'Prescription Pick-up',
+  'Online Care'
+]
+
+module.exports = {
+  amenities,
+};
