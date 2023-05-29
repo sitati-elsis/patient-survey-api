1515067071cba8aaa3c3706d93fd79dba1104f2a
sitati-elsis <sitatielsis@gmail.com>
2023-05-29 20:09:49 +0300

    implement filters for resources

--- src/controllers/reports.controller.js
+++ src/controllers/reports.controller.js
@@ -2,15 +2,50 @@ const catchAsync = require("../utils/catchAsync");
 const { reportsService } = require("../services");
 
 const getCampaignStatistics = catchAsync(async (req, res) => {
+  const {
+    campaignId,
+    startDate,
+    endDate,
+    practitionerId,
+    page = 1,
+    limit = 10,
+  } = req.query;
+  const organizationId = req.organizationId;
   const campaignStatistics = await reportsService.getCampaignStatistics(
-    req.organizationId
+    organizationId,
+    campaignId,
+    startDate,
+    endDate,
+    practitionerId,
+    page,
+    limit
   );
-  res.send(campaignStatistics);
+  const count = campaignStatistics.length;
+  res.send({
+    campaignStatistics,
+    totalPages: Math.ceil(count / parseInt(limit)),
+    currentPage: page,
+  });
 });
 
 const getPractionerEngagements = catchAsync(async (req, res) => {
-  const practionerEngangements =
-    await reportsService.getPractionerEngagements();
+  const {
+    startDate,
+    endDate,
+    practitionerId,
+    page = 1,
+    limit = 10,
+  } = req.query;
+  const organizationId = req.organizationId;
+
+  const practionerEngangements = await reportsService.getPractionerEngagements(
+    organizationId,
+    startDate,
+    endDate,
+    practitionerId,
+    page,
+    limit
+  );
 
   res.send(practionerEngangements);
 });
--- src/routes/v1/reports.route.js
+++ src/routes/v1/reports.route.js
@@ -1,14 +1,24 @@
 const express = require("express");
 const auth = require("../../middlewares/auth");
 const reportsControler = require("../../controllers/reports.controller");
+const validate = require("../../middlewares/validate");
+const { reportsValidation } = require("../../validations");
 
 const router = express.Router();
 
 router
-  .route("/")
-  .get(auth("viewAdminReports"), reportsControler.getCampaignStatistics);
+  .route("/campaignReports")
+  .get(
+    auth("viewAdminReports"),
+    validate(reportsValidation.getCampaignStatistics),
+    reportsControler.getCampaignStatistics
+  );
 
 router
   .route("/practionerReports")
-  .get(auth("viewAdminReports"), reportsControler.getPractionerEngagements);
+  .get(
+    auth("viewAdminReports"),
+    validate(reportsValidation.getPractionerEngagements),
+    reportsControler.getPractionerEngagements
+  );
 module.exports = router;
